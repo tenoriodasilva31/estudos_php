0d0a7b9246d58c1ea1010adf91d89cd7b8c578a7
Jonathan Tenório da Silva <jonathan.tenoriodev@gmail.com>
2023-10-29 23:11:39 -0300

    estudos de php

---
+++ .history/Modulo_1/sintaxe_20231029230751.php
@@ -0,0 +1,22 @@
+<?php
+
+//Isso é um comentário de uma linha
+
+
+/* 
+Esse é um comentário de múltiplas
+linhas
+
+*/
+
+$var1 = 10;
+$Var1 = 100;
+$_var1 = 200;
+/* As variaveis devem começar com "$" e terminar com ";" e são case sensitive
+Além disso podem começar com letra maiuscula ou underline após o $
+*/
+
+$var3 = "texto";
+$var3  = 'Texto'
+
+
---
+++ Modulo_1/sintaxe.php
@@ -0,0 +1,26 @@
+<?php
+
+//Isso é um comentário de uma linha
+
+
+/* 
+Esse é um comentário de múltiplas
+linhas
+
+*/
+
+$var1 = 10;
+$Var1 = 100;
+$_var1 = 200;
+/* As variaveis devem começar com "$" e terminar com ";" e são case sensitive
+Além disso podem começar com letra maiuscula ou underline após o $
+*/
+
+$var3 = "texto";
+$var3  = 'Texto'
+
+/* Variavéis de texto podem ser englobadas por aspas duplas ou simples.
+Caso haja html, ele deve ser escrito fora do php ou o php deve ser escrito dentro do html, sempre respeitando a abertura de tag do php 
+*/
+
+?>
